71dc3bfbe180222fa3c1665c7c84bd3f0b9305ea
Ricardo Malta <125774953+Ricardomms10@users.noreply.github.com>
2024-10-30 12:30:41 -0300

    dia3010

--- components/DataProject.tsx
+++ components/DataProject.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import Jokenpo from '../public/assets/JOKEPO.png';
 import AppAcademia from '../public/assets/CELULARACADEMIA.png';
 import Academia from '../public/assets/Academiacompleto.png';
-import TempoImage from '../public/assets/TEMPCOM.png';
-import EstacionamentoImage from '../public/assets/estacionamentoCTP.png';
 import PortifoImage from '../public/assets/PORTICOMPL.png';
+import Personal from '../public/assets/personaltela.png'
 import { StaticImageData } from 'next/image';
 
 export interface Project {
@@ -18,9 +16,9 @@ export interface Project {
 }
 
 export const data: Project[] = [
- 
+
   {
-    id: '2',
+    id: '1',
     image: Academia,
     nome: ' LANDING PAGE ACADEMIA',
     link: 'https://github.com/Ricardomms10/ProjetoAcademiaPrime',
@@ -28,39 +26,23 @@ export const data: Project[] = [
     ling: 'Next.JS, Scss/Sass',
   },
   {
-    id: '3',
-    image: TempoImage,
-    nome: 'CLIMA TEMPO API',
-    link: 'https://github.com/Ricardomms10/ClimaTempo',
-    descricao: 'Consome uma API Climática',
-    ling: 'React.JS, Styled Components',
-  },
-  {
-    id: '4',
-    image: EstacionamentoImage,
-    nome: 'ESTACIONAMENTO BATE-BATE',
-    link: 'https://github.com/Ricardomms10/EstacionamentoBate',
-    descricao: 'Sistema Online de entrada e saída de veículos.',
-    ling: 'React JS, Vite.JS, Styled Components e TypeScript',
-  },
-  {
-    id: '5',
-    image: Jokenpo,
-    nome: 'JOKENPÔ',
-    link: 'https://github.com/Ricardomms10/JOKENPO',
-    descricao: 'Um jogo divertido de pedra, papel e tesoura.',
-    ling: 'Next.js, TypeScript e Sass/Scss',
-  },
-  {
-    id: '6',
+    id: '2',
     image: PortifoImage,
-    nome: 'Essa Aplicação',
+    nome: 'ESSA APLICAÇÃO',
     link: 'https://github.com/Ricardomms10/PortfolioNew',
     descricao: 'Meu portfólio',
     ling: 'React JS, Styled Components e Material IU',
   },
   {
-    id: '1',
+    id: '3',
+    image: Personal,
+    nome: 'LANDING PAGE PERSONAL',
+    link: 'https://github.com/Ricardomms10',
+    descricao: 'Uma Landing Page para um Personal Fictício',
+    ling: 'NextJs, Scss/Sass',
+  },
+  {
+    id: '4',
     image: AppAcademia,
     nome: 'APP ACADEMIA',
     link: 'https://github.com/Ricardomms10/AppAcademia',
