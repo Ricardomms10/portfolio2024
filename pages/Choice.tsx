c7bc26effd807136ee526011f76f45bdad2c6709
Ricardo <Ricardomms10@gmail.com>
2025-06-13 16:26:28 -0300

    agora vai

--- pages/Choice.tsx
+++ pages/Choice.tsx
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCode } from '@fortawesome/free-solid-svg-icons';
 import { faMagnifyingGlassChart } from '@fortawesome/free-solid-svg-icons';
 import style from '../styles/Choice.module.scss';
+import Link from 'next/link';
 
 interface ChoiceProps { }
 
@@ -12,11 +13,11 @@ const Choice: React.FC<ChoiceProps> = () => {
             <div className={style.buttonGroup}>
                 <button>
                     <FontAwesomeIcon icon={faCode} style={{ marginRight: '0.5rem' }} />
-                    <a href="/Projeto"> Web Developer</a>
+                    <Link href="/Projeto"> Web Developer</Link>
                 </button>
                 <button>
                     <FontAwesomeIcon icon={faMagnifyingGlassChart} />
-                    <a href="/Projeto"> Data Analyst</a>
+                    <Link href="/Projeto"> Data Analyst</Link>
                 </button>
             </div>
         </div>
